31f8353d5d372739a8a3a861fa9c3e8889f90db5
Ariel Azarcon <aazarcon@ad.acquireap>
2018-05-22 18:55:43 +0800

    trying to remove latest Posts LOL part2

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -10,9 +10,9 @@
     {!! get_search_form(false) !!}
   @endif -->
 
-  @while (have_posts()) @php the_post() @endphp
+<!--   @while (have_posts()) @php the_post() @endphp
     @include('partials.content-'.get_post_type())
-  @endwhile
+  @endwhile -->
 
   {!! get_the_posts_navigation() !!}
 @endsection
