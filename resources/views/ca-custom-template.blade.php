a6cd0aafc15b2b258eb0856c0da9cfe6a5c1bcd2
Ariel Azarcon <aazarcon@ad.acquireap>
2018-05-31 19:28:27 +0800

    fixing embedding issues on server WP. fixing because Im not yet sure if it will be fixed after this

--- resources/views/ca-custom-template.blade.php
+++ resources/views/ca-custom-template.blade.php
@@ -16,10 +16,15 @@
 		    	<h1 class="text-left">{!! App::title() !!}</h1>
 		    	@include('partials.content-page')
 
-          <h1>
-            
-            <!-- {!! get_the_ID() !!} -->
-          </h1>
+        
+          <!-- cant display iframe on server.
+          so to display an embedded code, i have to check if the page being displayed is the location page.
+          if it is, ill display he widget
+           -->
+          @if(is_page( 25 )) 
+            <div class="mapouter"><div class="gmap_canvas"><iframe width="858" height="500" id="gmap_canvas" src="https://maps.google.com/maps?q=111-115 N.W. SEVENTH ST., RICHMOND, IN 47374 &t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe></div><style>.mapouter{text-align:right;height:500px;width:858px;}.gmap_canvas {overflow:hidden;background:none!important;height:500px;width:858px;}</style></div>
+          @endif
+
     		</div>
     	</div>
     </div>
